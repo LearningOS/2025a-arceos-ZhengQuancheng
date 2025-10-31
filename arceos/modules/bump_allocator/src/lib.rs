fac137d61e5e3f53150131bf279dfe0ace15f2cf
ZhengQuancheng <zhengqc@yeah.net>
2025-10-31 20:16:53 +0800

    solve `alt_alloc`

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -16,17 +16,33 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    start: usize,      // 内存区域起始位置
+    end: usize,        // 内存区域结束位置
+    b_pos: usize,      // 字节分配的位置指针
+    p_pos: usize,      // 页分配的位置指针
+    count: usize,      // 记录字节分配的次数
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            end: 0,
+            b_pos: 0,
+            p_pos: 0,
+            count: 0,
+        }
     }
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.start = start;
+        self.end = start + size;
+        self.b_pos = start;
+        self.p_pos = self.end;
+        self.count = 0;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
@@ -39,23 +55,44 @@ impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        // 计算对齐后的分配位置
+        let align = layout.align();
+        let size = layout.size();
+        let aligned_pos = (self.b_pos + align - 1) & !(align - 1);
+        let new_b_pos = aligned_pos + size;
+        // 检查是否有足够的空间
+        if new_b_pos > self.p_pos {
+            return Err(allocator::AllocError::NoMemory);
+        }
+        // 更新分配位置和计数器
+        self.b_pos = new_b_pos;
+        self.count += 1;
+        // 返回分配的内存地址
+        core::ptr::NonNull::new(aligned_pos as *mut u8).ok_or(allocator::AllocError::NoMemory)
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        if self.count > 0 {
+            // 更新计数器
+            self.count -= 1;
+            // 所有分配的字节都被释放
+            if self.count == 0 {
+                // 重置分配位置
+                self.b_pos = self.start;
+            }
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!()
+        self.end - self.start
     }
 
     fn used_bytes(&self) -> usize {
-        todo!()
+        (self.b_pos - self.start) + (self.end - self.p_pos)
     }
 
     fn available_bytes(&self) -> usize {
-        todo!()
+        self.p_pos - self.b_pos
     }
 }
 
@@ -67,7 +104,17 @@ impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
         num_pages: usize,
         align_pow2: usize,
     ) -> allocator::AllocResult<usize> {
-        todo!()
+        // 计算对齐后的分配位置
+        let align = 1 << align_pow2;
+        let size = num_pages * Self::PAGE_SIZE;
+        let aligned_pos = (self.p_pos - size) & !(align - 1);
+        // 检查是否有足够的空间
+        if aligned_pos < self.b_pos {
+            return Err(allocator::AllocError::NoMemory);
+        }
+        // 更新分配位置
+        self.p_pos = aligned_pos;
+        Ok(aligned_pos)
     }
 
     fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
@@ -75,14 +122,14 @@ impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
     }
 
     fn total_pages(&self) -> usize {
-        todo!()
+        (self.end - self.start) / Self::PAGE_SIZE
     }
 
     fn used_pages(&self) -> usize {
-        todo!()
+        (self.end - self.p_pos) / Self::PAGE_SIZE
     }
 
     fn available_pages(&self) -> usize {
-        todo!()
+        (self.p_pos - self.b_pos) / Self::PAGE_SIZE
     }
 }
